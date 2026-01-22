ff7b5f053868970aa9fcbedb9187b608fb0c4e16
Ktaewon <dydalehdxo@naver.com>
2026-01-23 01:39:25 +0900

    feat: add Google Adsense integration

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -6,6 +6,7 @@ import { Header } from "@/components/layout/Header";
 import { Footer } from "@/components/layout/Footer";
 import { NextIntlClientProvider } from 'next-intl';
 import { getMessages } from 'next-intl/server';
+import GoogleAdsense from "@/components/common/GoogleAdsense";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -26,6 +27,9 @@ export default async function RootLayout({
 
   return (
     <html lang={locale} suppressHydrationWarning>
+      <head>
+        <GoogleAdsense pId="0000000000000000" />
+      </head>
       <body className={inter.className}>
         <NextIntlClientProvider messages={messages}>
           <ThemeProvider
